73bd5c9a2749fb5c5b2784a8df0ac61e6df0df72
Jean-Vincent D'ADDA <jean-vincent.d-adda@vectaury.io>
2017-11-16 18:15:54 +0100

    Ask location permission when app is started

--- vectaury-kotlin/src/main/java/io/vectaury/android/samples/kotlin/MainActivity.kt
+++ vectaury-kotlin/src/main/java/io/vectaury/android/samples/kotlin/MainActivity.kt
@@ -1,12 +1,41 @@
 package io.vectaury.android.samples.kotlin
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val PERMISSION_REQUEST_LOCATION = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        askLocationPermission()
+    }
+
+    private fun askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_REQUEST_LOCATION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Location permission granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
--- vectaury/src/main/java/io/vectaury/android/samples/java/MainActivity.java
+++ vectaury/src/main/java/io/vectaury/android/samples/java/MainActivity.java
@@ -1,13 +1,42 @@
 package io.vectaury.android.samples.java;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askLocationPermission();
+    }
+
+    private void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Location permission granted", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 }
